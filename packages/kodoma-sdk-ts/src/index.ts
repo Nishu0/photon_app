ee1efd05bfacc5c040d6252744f8636d2b82f499
Nishu0 <itsthakkarnisarg@gmail.com>
2026-05-06 16:47:33 +0530

    sdk: public entry barrel

---
+++ packages/kodoma-sdk-ts/src/index.ts
@@ -0,0 +1,20 @@
+export { Kodama } from "./runtime";
+export { definePlatform } from "./definePlatform";
+export { text, attachment, custom, resolveContents } from "./content";
+export type {
+  AttachmentContent,
+  Content,
+  ContentBuilder,
+  ContentInput,
+  CustomContent,
+  KodamaInstance,
+  Message,
+  PlatformDef,
+  PlatformNarrower,
+  PlatformProviderConfig,
+  ProviderActions,
+  ProviderMessage,
+  Space,
+  TextContent,
+  User
+} from "./types";
